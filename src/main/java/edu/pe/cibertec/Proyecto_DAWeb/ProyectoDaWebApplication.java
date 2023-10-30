879316acad0c494eb9acc6d1da245c02617efaaf
Alfredo <alfredoqooo@gmail.com>
2023-10-30 18:11:26 -0500

    init comit

---
+++ src/main/java/edu/pe/cibertec/Proyecto_DAWeb/ProyectoDaWebApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.Proyecto_DAWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/Proyecto_DAWeb/ProyectoDaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.Proyecto_DAWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
